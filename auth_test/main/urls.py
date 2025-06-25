4d0c965a593771d82921d26b2b44ae09729fd53d
logg1n <loggiin13@gmail.com>
2025-06-25 22:01:30 +0300

    Initial commit

--- auth_test/main/urls.py
+++ auth_test/main/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', next_page='index'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
